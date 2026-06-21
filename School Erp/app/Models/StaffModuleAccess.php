50e462f3dfc666699d89dcd3890567bb000b6ce9
Itzdip190 <Souhardyadip.mondal999@gmail.com>
2026-06-21 19:18:22 +0530

    fix: explicit table name staff_module_access on StaffModuleAccess model to resolve 500 error due to staff_module_accesses table missing

--- School Erp/app/Models/StaffModuleAccess.php
+++ School Erp/app/Models/StaffModuleAccess.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class StaffModuleAccess extends Model
 {
+    protected $table = 'staff_module_access';
+
     protected $fillable = [
         'school_id', 'user_id', 'module_key', 'feature_key', 'view_access', 'edit_access',
     ];
